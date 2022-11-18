6d8d016bb1cbcfd5739e592ec6435095f5be7eb4
UribeAlejandro <jauriberamirez@gmail.com>
2022-11-17 23:55:36 -0300

    Baseline model and readme

--- utils/ExperimentLogger.py
+++ utils/ExperimentLogger.py
@@ -3,20 +3,27 @@ import os
 import pickle
 from datetime import datetime as dt
 
+from pandas import DataFrame, Series
 from sklearn.metrics import accuracy_score, fbeta_score
 
 
 class ExperimentLogger:
-
-    def __init__(self, X_train, X_test, y_train, y_test, model):
+    def __init__(
+        self,
+        X_train: DataFrame,
+        X_test: DataFrame,
+        y_train: Series,
+        y_test: Series,
+        model,
+    ) -> None:
         self.X_train = X_train
         self.X_test = X_test
         self.y_train = y_train
         self.y_test = y_test
         self.model = model
-        self.parent_folder = f'experiment/{dt.now()}'
+        self.parent_folder = f"experiment/my_own_private_tracker/{dt.now()}"
 
-    def log_experiment(self):
+    def log_experiment(self) -> None:
 
         try:
             os.makedirs(self.parent_folder)
@@ -27,39 +34,40 @@ class ExperimentLogger:
         self.log_parameters()
         self.store_model()
 
-
-
     def log_metrics(self):
         metrics = {
             "training": {
-                "accuracy_score": accuracy_score(self.y_train,
-                                                 self.model.predict(self.X_train)),
-                "f_beta_avg_macro": fbeta_score(self.y_train,
-                                      self.model.predict(self.X_train),
-                                      beta=2,
-                                      average='macro')
+                "accuracy_score": accuracy_score(
+                    self.y_train, self.model.predict(self.X_train)
+                ),
+                "f_beta_avg_macro": fbeta_score(
+                    self.y_train,
+                    self.model.predict(self.X_train),
+                    beta=2,
+                    average="macro",
+                ),
             },
             "test": {
-                "accuracy_score": accuracy_score(self.y_test,
-                                                 self.model.predict(self.X_test)),
-                "f_beta_avg_macro": fbeta_score(self.y_test,
-                                      self.model.predict(self.X_test),
-                                      beta=2,
-                                      average='macro')
-            }
+                "accuracy_score": accuracy_score(
+                    self.y_test, self.model.predict(self.X_test)
+                ),
+                "f_beta_avg_macro": fbeta_score(
+                    self.y_test,
+                    self.model.predict(self.X_test),
+                    beta=2,
+                    average="macro",
+                ),
+            },
         }
 
-        with open(f'{self.parent_folder}/metrics.json', "w") as f:
+        with open(f"{self.parent_folder}/metrics.json", "w") as f:
             json.dump(metrics, f)
-        f.close()
 
     def log_parameters(self):
         params = self.model.get_params()
-        with open(f'{self.parent_folder}/params.json', "w") as f:
+        with open(f"{self.parent_folder}/params.json", "w") as f:
             json.dump(params, f)
-        f.close()
-
 
     def store_model(self):
-        with open(f'{self.parent_folder}/model.pkl', 'wb') as f:
+        with open(f"{self.parent_folder}/model.pkl", "wb") as f:
             pickle.dump(self.model, f)
--- utils/training.py
+++ utils/training.py
@@ -1,3 +1,6 @@
+import os
+from typing import Tuple
+
 import mlflow
 from sklearn.metrics import accuracy_score, fbeta_score
 
@@ -5,26 +8,29 @@ from sklearn.metrics import accuracy_score, fbeta_score
 # 1
 def experiment_low_level(X_train, X_test, y_train, y_test, model):
     acc_score_train = accuracy_score(y_train, model.predict(X_train))
-    f_beta_train = fbeta_score(y_train, model.predict(X_train), beta=2, average='macro')
+    f_beta_train = fbeta_score(
+        y_train, model.predict(X_train), beta=2, average="macro"
+    )
 
     acc_score_test = accuracy_score(y_test, model.predict(X_test))
-    f_beta_test = fbeta_score(y_test, model.predict(X_test), beta=2, average='macro')
-
+    f_beta_test = fbeta_score(
+        y_test, model.predict(X_test), beta=2, average="macro"
+    )
 
     with mlflow.start_run() as run:
         metrics = {
-            "accuracy_score_train": accuracy_score(y_train,
-                                                 model.predict(X_train)),
-            "f_beta_avg_macro_train": fbeta_score(y_train,
-                                                model.predict(X_train),
-                                                beta=2,
-                                                average='macro'),
-            "accuracy_score_test": accuracy_score(y_test,
-                                                 model.predict(X_test)),
-            "f_beta_avg_macro_test": fbeta_score(y_test,
-                                                model.predict(X_test),
-                                                beta=2,
-                                        average='macro')
+            "accuracy_score_train": accuracy_score(
+                y_train, model.predict(X_train)
+            ),
+            "f_beta_avg_macro_train": fbeta_score(
+                y_train, model.predict(X_train), beta=2, average="macro"
+            ),
+            "accuracy_score_test": accuracy_score(
+                y_test, model.predict(X_test)
+            ),
+            "f_beta_avg_macro_test": fbeta_score(
+                y_test, model.predict(X_test), beta=2, average="macro"
+            ),
         }
         mlflow.log_metrics(metrics)
         mlflow.log_params(model.get_params())
@@ -35,12 +41,14 @@ def experiment_low_level(X_train, X_test, y_train, y_test, model):
 
 
 # 2
-def experiment_auto_logger(X_train, X_test, y_train, y_test, model):
+def experiment_auto_logger(X_train, X_test, y_train, y_test, model) -> None:
 
     mlflow.sklearn.autolog()
 
     with mlflow.start_run() as run:
         model.fit(X_train, y_train)
-        mlflow.sklearn.eval_and_log_metrics(model, X_test, y_test, prefix="val_")
+        mlflow.sklearn.eval_and_log_metrics(
+            model, X_test, y_test, prefix="val_"
+        )
 
     mlflow.end_run()
